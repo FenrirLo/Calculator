ecca084036acc8feff6fe71e975c3dad04d59938
Fen Lombardi and Ryan McLean <ryan@>
2019-01-14 13:35:44 -0800

    started basic scripts.html moved over first part of calc into scripts.js created styles.css

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(number1, number2) {
+	return number1 + number2;
+};
+
+var number1 = parseInt(prompt("What's your first number you want to insert to add"));
+var number2 = parseInt(prompt("Please select your second number you want to add."));
+
+alert(add(number1, number2));
