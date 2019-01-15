726d96a3a7a3696a21a8b3ec2c3e090425be23e2
Fen Lombardi and Ryan McLean <fen+ryan@>
2019-01-14 16:09:49 -0800

    added particles and more functions to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,7 @@ var divide = function(number1, number2) {
 var number1 = parseInt(prompt("What's your first number you want to insert to add"));
 var number2 = parseInt(prompt("Please select your second number you want to add."));
 
-var result = divide(number1, number2);
+var result = add(number1, number2);
 alert(result);
 
 //BMI Calculator
